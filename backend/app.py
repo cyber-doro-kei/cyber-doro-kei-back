a02c93f68b24b8754a0a44992eeb0e5f280ba250
kawachan <inheritans1026@gmail.com>
2024-03-18 16:47:57 +0900

    delete-不要な条件チェックの削除

--- backend/app.py
+++ backend/app.py
@@ -25,69 +25,61 @@ async def hello():
 
 @app.post("/start/assign/{room_id}")
 async def assign_member(room_id: str):
-    if room_id == None:
-        response = {"response": "Invalid input"}
-        return JSONResponse(status_code=405, content=response)
-    else:
-        try:
-            # usersコレクションの参照を取得
-            users_ref = db.collection("users")
-            
-            #copの設定人数を取り出す
-            doc_ref = db.collection("rooms").document(room_id)
-            doc_snapshot = doc_ref.get()
-            cop_ration = doc_snapshot.get("cop_num")
-            robber_ration = doc_snapshot.get("robber_num")
-            
-            # ドキュメントを取得し、room_idフィールドが指定されたroom_idと等しい場合はis_copフィールドを更新する
-            users = users_ref.where("room_id", "==", room_id).stream()
-            users_list = list(users)
-            users_num = len(users_list)
-            # userをシャッフルする
-            random.shuffle(users_list)
-            
-            #警察にいれる人数を計算
-            cop_num = math.floor((cop_ration / (cop_ration + robber_ration)) * users_num)
-            
-            for user in users_list:
-                user_ref = users_ref.document(user.id)
-                if cop_num > 0:
-                    user_ref.update({"is_cop": True})
-                    cop_num -= 1
-                else:
-                    user_ref.update({"is_cop": False})
-            
-            response = {"response": "is_cop field updated successfully for matching documents"}
-            return JSONResponse(status_code=200, content=response)
+    try:
+        # usersコレクションの参照を取得
+        users_ref = db.collection("users")
+        
+        #copの設定人数を取り出す
+        doc_ref = db.collection("rooms").document(room_id)
+        doc_snapshot = doc_ref.get()
+        cop_ration = doc_snapshot.get("cop_num")
+        robber_ration = doc_snapshot.get("robber_num")
+        
+        # ドキュメントを取得し、room_idフィールドが指定されたroom_idと等しい場合はis_copフィールドを更新する
+        users = users_ref.where("room_id", "==", room_id).stream()
+        users_list = list(users)
+        users_num = len(users_list)
+        # userをシャッフルする
+        random.shuffle(users_list)
+        
+        #警察にいれる人数を計算
+        cop_num = math.floor((cop_ration / (cop_ration + robber_ration)) * users_num)
+        
+        for user in users_list:
+            user_ref = users_ref.document(user.id)
+            if cop_num > 0:
+                user_ref.update({"is_cop": True})
+                cop_num -= 1
+            else:
+                user_ref.update({"is_cop": False})
+        
+        response = {"response": "is_cop field updated successfully for matching documents"}
+        return JSONResponse(status_code=200, content=response)
 
-        except Exception as e:
-            # エラーが発生した場合はHTTP例外を発生させる
-            raise HTTPException(status_code=500, detail=f"Error updating documents: {str(e)}")
+    except Exception as e:
+        # エラーが発生した場合はHTTP例外を発生させる
+        raise HTTPException(status_code=500, detail=f"Error updating documents: {str(e)}")
 
 @app.post("/start/timer/{room_id}")
 #room_idは
 async def start_timer(room_id: str, req: StartTimer):
-    if room_id == None:
-        response = {"response": "Invalid input"}
-        return JSONResponse(status_code=405, content=response)
-    else:
-        try:
-            # ドキュメントに追加するデータを準備
-            started_at = datetime.now(jst).isoformat()
-            data = {
-                "started_at": started_at,
-                "is_active" : True
-            }
+    try:
+        # ドキュメントに追加するデータを準備
+        started_at = datetime.now(jst).isoformat()
+        data = {
+            "started_at": started_at,
+            "is_active" : True
+        }
 
-            # Firebaseのroomsコレクションへの参照を取得し、指定されたドキュメントにデータを追加
-            doc_ref = db.collection("rooms").document(room_id)
-            doc_ref.update(data)
+        # Firebaseのroomsコレクションへの参照を取得し、指定されたドキュメントにデータを追加
+        doc_ref = db.collection("rooms").document(room_id)
+        doc_ref.update(data)
 
-            command = ['python','event/execute.py', room_id]
-            subprocess.Popen(command) # COMMENT: サブプロセスでDB監視を実施
+        command = ['python','event/execute.py', room_id]
+        subprocess.Popen(command) # COMMENT: サブプロセスでDB監視を実施
 
-            return {"message": "Data added to Firebase successfully"}
+        return {"message": "Data added to Firebase successfully"}
 
-        except Exception as e:
-            # エラーが発生した場合はHTTP例外を発生させる
-            raise HTTPException(status_code=500, detail=f"Error adding data to Firebase: {str(e)}")
+    except Exception as e:
+        # エラーが発生した場合はHTTP例外を発生させる
+        raise HTTPException(status_code=500, detail=f"Error adding data to Firebase: {str(e)}")
