85f07ac89b002ab803b28395c8ba57ef9a6c18f4
kawachan <inheritans1026@gmail.com>
2024-03-18 15:41:31 +0900

    add-executeファイル作成

--- backend/event/event.py
+++ backend/event/event.py
@@ -1,4 +1,5 @@
-from fastapi.responses import JSONResponse
+import time
+from datetime import datetime, timedelta
 
 class Event:
     """
@@ -67,15 +68,40 @@ class Event:
 
     def check_db(self) -> bool:
         """
-        description: 処理実行関数
+        description: データベースを確認しにいく処理
         -------------------
         none
         -------------------
         return: boolean -> イベントを開始したか否かのフラグ Trueの場合DBの監視を停止する
         """
-
+        
         if self.is_event_start():
             self.add_event_logs()
             return True
         else:
             return False
+        
+    def main(self) -> None:
+        """
+        description: 処理実行関数
+        -------------------
+        none
+        -------------------
+        return: none
+        """
+
+        doc_ref = self.db.collection("rooms").document(self.room_id)
+        doc_snapshot = doc_ref.get()
+        play_time_seconds = doc_snapshot.get("play_time_seconds")
+
+        start_time: datetime = datetime.now()
+        end_time: datetime = start_time + timedelta(seconds = play_time_seconds)
+        # COMMENT: プレイ時間を超えた場合、強制的にDBの監視を停止する
+        while end_time > datetime.now():
+            is_finish: bool = self.check_db()
+            if is_finish: # COMMENT: eventが発令されたらループを抜ける
+                break
+            time.sleep(60) # COMMENT: 60秒置きに実行
+        
+        print("Event is started")
+            
